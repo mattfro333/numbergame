e5266ca0a7b96ae825867f8c4f3f5769dd5a1d60
mattfro333 <mattfro333@gmail.com>
2019-05-27 16:16:05 -0600

    basic guessing game

--- number.js
+++ number.js
@@ -1,5 +1,13 @@
 var secretNumber = 4;
 
-var guess = prompt("Guess a number");
+var guess = Number(prompt("Guess a number"));
 
 alert(guess);
+
+if(guess === secretNumber) {
+  alert("YOU GOT IT RIGHT!!")
+} else if(guess > secretNumber) {
+  alert("Too high. Guess again!")
+} else {
+  alert("Too low. Guess again!")
+}
