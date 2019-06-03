3c0d14a46303c40c29e48060481905f52a9c6bd0
mattfro333 <mattfro333@gmail.com>
2019-06-03 17:27:24 -0600

    color func

--- number.js
+++ number.js
@@ -1,4 +1,13 @@
 var secretNumber = 4;
+const colors = ['blue','green','black','purple','orange','red','brown','violet','yellow','indigo'];
+
+function colorGuess(colors){
+  for(var i = 0; i <= colors.length; i++){
+    if(prompt("Whats your favorite color?") === colors[i]){
+      prompt("Whats the air speed velocity of a laiden swallow?")
+    }
+  }
+}
 
 var stringGuess = prompt("Guess a number");
 var guess = Number(stringGuess);
