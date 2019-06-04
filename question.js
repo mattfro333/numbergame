14ee915c2c133def08ed0f6b2788ceed465f61df
mattfro333 <mattfro333@gmail.com>
2019-06-04 14:14:29 -0600

    Created the framework for the questions

---
+++ question.js
@@ -0,0 +1,26 @@
+
+const colors = ['blue','green','black','purple','orange','red','brown','violet','yellow','indigo'];
+
+let name = prompt("What is your name?")
+
+let colorGuess = prompt("What is your favorite color?")
+let guess = colorGuess
+let questGuess = prompt("What is your quest?")
+let quest = questGuess
+
+if(name != null){
+  colorGuess
+}
+
+  for(var i = 0; i <= colors.length; i++){
+    if(guess === colors[i]){
+      console.log(colors[i])
+     questGuess
+   }
+  }
+
+
+
+if(quest === "I seek the grail"){
+  alert("You may pass.")
+}
